87cbb52299cd58b611a42b1b9b13e70ef4081cd4
Joseph Tzeng <joseph@pushcorn.com>
2022-08-26 19:08:27 -0400

    adds keep_fargs support via comments

--- test/tests/lib/ui/js/UglifyJs.js
+++ test/tests/lib/ui/js/UglifyJs.js
@@ -8,4 +8,29 @@ test.method ("ui.js.UglifyJs", "minify")
     .given (nit.readFile ("test/resources/example.js") + "}}")
     .throws ("error.process_failure")
     .commit ()
+
+    .should ("keep the func arg names if /* @keep_fargs */ was added before the func decl")
+    .given ("/* @keep_fargs */ function test (nit, app) {}")
+    .returns ("function test(nit,app){}")
+    .commit ()
+
+    .should ("keep the func arg names if /* @keep_fargs */ was added after the function keyword")
+    .given ("function /* @keep_fargs */ test (nit, app) {}")
+    .returns ("function test(nit,app){}")
+    .commit ()
+
+    .should ("keep the func arg names if /* @keep_fargs */ was added before func arg decl")
+    .given ("function test (/* @keep_fargs */ nit, app) {}")
+    .returns ("function test(nit,app){}")
+    .commit ()
+
+    .should ("keep the func arg names if /* @keep_fargs */ was added after func arg decl")
+    .given ("function test (nit, app /* @keep_fargs */) {}")
+    .returns ("function test(nit,app){}")
+    .commit ()
+
+    .should ("keep the func arg names if /* @keep_fargs */ was added after func arg decl")
+    .given ("module.exports = function (nit, app /* @keep_fargs */) {};")
+    .returns ("module.exports=function(nit,app){};")
+    .commit ()
 ;
