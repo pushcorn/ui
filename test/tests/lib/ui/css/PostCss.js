3c854926fec6dae46ea01fe89d3b83477ad454fc
Joseph Tzeng <joseph@pushcorn.com>
2023-07-08 14:47:06 -0400

    updates the nit dependency

--- test/tests/lib/ui/css/PostCss.js
+++ test/tests/lib/ui/css/PostCss.js
@@ -1,9 +1,9 @@
 test.object ("ui.css.PostCss")
     .should ("be able to transform CSS content with PostCSS")
     .given ("postcss-nested")
-    .expectingMethodToReturnValue ("result.transform", `div.highlighted a {
+    .expectingMethodToReturnValue ("result.transform", nit.readFile ("test/resources/example.css"), `div.highlighted a {
         color: red;
     }
-`, nit.readFile ("test/resources/example.css"))
+`)
     .commit ()
 ;
